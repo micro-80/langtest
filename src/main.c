102ab3f8dd19b2d1851af9703c62b623773d084e
micro80 <me@micro80.com>
2026-01-02 10:27:43 +0000

    Read file line by line
    Use fgets() now instead of reading character by character. Also
    introduces command line args and better error messaging, as well as an
    outline of the AST enum

--- src/main.c
+++ src/main.c
@@ -1,23 +1,50 @@
+#include <errno.h>
 #include <stdio.h>
+#include <string.h>
 
-const char* FILE_NAME = "test.lt";
+enum AST {
+  KEYWORD_FUNCTION,
+  FUNCTION_DECLARATION_NAME,
+  FUNCTION_DECLARATION_RETURN_INDICATOR,
+  FUNCTION_DECLARATION_RETURN_TYPE,
+  CURLY_BRACKET_OPEN,
+  CURLY_BRACKET_CLOSE,
+  CURLY_BRACE_OPEN,
+  CURLY_BRACE_CLOSE,
+  KEYWORD_DEF,
+  VARIABLE_DECLARATION_NAME,
+  VARIABLE_DECLARATION_EQUALS,
+  STRING_LITERAL,
+  SEMI_COLON,
+  CALL_FUNCTION,
+  KEYWORD_RETURN,
+  NUMERIC_LITERAL
+};
 
-int main() {
-  FILE *testSrc = fopen(FILE_NAME, "r");
-  if (!testSrc) {
-    perror(FILE_NAME);
+int main(int argc, char **argv) {
+  if (argc != 2) {
+    fprintf(stderr,
+            "You need to supply exactly 1 argument - the file to be parsed.\n");
     return 1;
   }
 
-  int character;
-  do {
-    character = getc(testSrc);
-    printf("%c\n", character);
-  } while (character != EOF);
+  const char* fileName = argv[1];
+  
+  FILE *testSrc = fopen(fileName, "r");
+  if (!testSrc) {
+    fprintf(stderr, "Error opening file %s: %s\n", fileName, strerror(errno));
+    return 1;
+  }
+
+  char buf[128];
+  while (fgets(buf, sizeof(buf), testSrc) != NULL) {
+    printf("%s\n", buf);
+  };
 
   if (fclose(testSrc) == EOF) {
-    printf("Error while closing file.");
+    perror("Error closing file");
+    return 1;
   }
-  
+
   return 0;
 }
